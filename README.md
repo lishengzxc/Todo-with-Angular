37c9f94d984ba08e6e839ddfb8973b4c3f2a27e3
lishengzxc <eric@lishengcn.cn>
2015-07-10 16:12:47 +0800

    完成目录构建

--- README.md
+++
@@ -1,2 +0,0 @@
-#ToDo with Angular(no 2.x)
-DEMO: http://lishengzxc.github.io/Todo-with-Angular/
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('todoWithAngular', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.bootstrap']);
+
+})();
