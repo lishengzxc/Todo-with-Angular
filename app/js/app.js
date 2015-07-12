9f9f4f9c9c6f31aa26e6333f1aa3684865ef72ce
lishengzxc <eric@lishengcn.cn>
2015-07-12 19:25:17 +0800

    update

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,13 @@
-var todo = angular.module('todo', []);
+var todo = angular.module('todo', ['ngRoute'])
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'tpl/main.html',
+				controller: 'mainController'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
+
+
--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,3 +1,32 @@
-/**
- * Created by lisheng on 15/7/12.
- */
+todo.controller('mainController', ['$scope', function ($scope) {
+	//$scope.category = {
+	//	id: 0,
+	//	name: '分类名'
+	//};
+	//
+	//$scope.task = {
+	//	id: 0,
+	//	ctn: '.....',
+	//	endtime: new Date(),
+	//	priority: null,
+	//	isDone: false
+	//}
+
+	function Category(name) {
+
+		this.name = name;
+	}
+
+
+
+
+
+	function Task(name, content, categoryId, endTime, priority) {
+
+		this.name = name;
+		this.content = content;
+		this.categoryId = categoryId;
+		this.endTime = endTime;
+		this.priority = priority;
+	}
+}]);
