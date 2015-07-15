33997525c1c4d5ed25235983bd16cc6d51d3c226
lishengzxc <eric@lishengcn.cn>
2015-07-15 14:54:14 +0800

    update

--- app/js/controllers/TaskController.js
+++ app/js/controllers/TaskController.js
@@ -3,44 +3,44 @@
  */
 todo.controller('TaskController', ['$scope', 'categoryListService', 'taskListService', function ($scope, categoryListService, taskListService) {
 
-	$scope.confirmAddTask = function ($event) {
-		var getNewTaskId = function () {
-			if (!$scope.taskList.length) {
-				return 0;
-			} else {
-				return $scope.taskList[$scope.taskList.length - 1].id + 1;
-			}
-		};
-		var resetNowTask = function () {
-			for (var i in $scope.nowTask) {
-				delete $scope.nowTask[i];
-			}
-			$scope.nowTask.priority = 0;
-		};
+  $scope.confirmAddTask = function ($event) {
+    var getNewTaskId = function () {
+      if (!$scope.taskList.length) {
+        return 0;
+      } else {
+        return $scope.taskList[$scope.taskList.length - 1].id + 1;
+      }
+    };
+    var resetNowTask = function () {
+      for (var i in $scope.nowTask) {
+        delete $scope.nowTask[i];
+      }
+      $scope.nowTask.priority = 0;
+    };
 
-		var cloneTask = function () {
-			var obj = {};
-			for (var i in $scope.nowTask) {
-				if ($scope.nowTask.hasOwnProperty(i)) {
-					obj[i] = $scope.nowTask[i];
-				}
-			}
-			return obj;
-		};
+    var cloneTask = function () {
+      var obj = {};
+      for (var i in $scope.nowTask) {
+        if ($scope.nowTask.hasOwnProperty(i)) {
+          obj[i] = $scope.nowTask[i];
+        }
+      }
+      return obj;
+    };
 
-		if (!$scope.nowTask.id) {
-			var _date = new Date();
-			$scope.nowTask.startTime = _date.getFullYear() + '-' + (_date.getMonth() + 1) + '-' + _date.getDate();
-			$scope.nowTask.categoryId = $scope.nowCategoryId;
-			$scope.nowTask.id = getNewTaskId();
-			$scope.taskList.push(cloneTask());
-			//$scope.nowTaskList.push(cloneTask());
-		} else {
-			$scope.taskList[$scope.nowTask.id] = cloneTask();
-			//$scope.nowTaskList[$scope.nowTask.id] = cloneTask();
-		}
-		taskListService.save($scope.taskList);
-		resetNowTask();
+    if (!$scope.nowTask.id) {
+      var _date = new Date();
+      $scope.nowTask.startTime = _date.getFullYear() + '-' + (_date.getMonth() + 1) + '-' + _date.getDate();
+      $scope.nowTask.categoryId = $scope.nowCategoryId;
+      $scope.nowTask.id = getNewTaskId();
+      $scope.taskList.push(cloneTask());
+      //$scope.nowTaskList.push(cloneTask());
+    } else {
+      $scope.taskList[$scope.nowTask.id] = cloneTask();
+      //$scope.nowTaskList[$scope.nowTask.id] = cloneTask();
+    }
+    taskListService.save($scope.taskList);
+    resetNowTask();
 
-	}
+  }
 }]);
